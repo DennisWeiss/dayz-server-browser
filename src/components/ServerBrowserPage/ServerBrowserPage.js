2c29f3805a56009606e2ee4dd41fab4b2e709bd4
Dennis Weiss <weiss.dennis98@yahoo.de>
2018-12-20 15:33:37 +0100

    Implementing server browser table

--- src/components/ServerBrowserPage/ServerBrowserPage.js
+++ src/components/ServerBrowserPage/ServerBrowserPage.js
@@ -2,6 +2,22 @@ import React from 'react'
 import ServersTable from '../ServersTable/ServersTable'
 import ServerBrowserFilter from '../ServerBrowserFilter/ServerBrowserFilter'
 import {getAllDayZServers} from '../../requests/dayz-servers'
+import './ServerBrowserPage.css'
+
+
+const mapServer = server => {
+  const gameTypeLst = server.gametype ? server.gametype.split(',') : []
+
+  return {
+    name: server.name,
+    currentPlayers: server.players,
+    maxPlayers: server.max_players,
+    dayTime: gameTypeLst.length > 0 ? gameTypeLst[gameTypeLst.length - 1] : '',
+    ip: `${server.addr.split(':')[0]}:${server.gameport}`,
+    version: server.version,
+    privHive: gameTypeLst.includes('privHive')
+  }
+}
 
 
 class ServerBrowserPage extends React.Component {
@@ -11,13 +27,17 @@ class ServerBrowserPage extends React.Component {
   }
 
   componentDidMount() {
-    getAllDayZServers().then(res => this.setState({servers: res.data}))
+    getAllDayZServers().then(res => {
+      if (res.data && res.data.response && res.data.response) {
+        this.setState({servers: res.data.response.servers.map(mapServer)})
+      }
+    })
   }
 
   render() {
     return (
-      <div>
-        <ServersTable servers={this.state.servers}/>
+      <div className="server-browser-page">
+        <ServersTable servers={this.state.servers.filter(server => !server.privHive)}/>
         <ServerBrowserFilter/>
       </div>
     )
--- src/components/ServersTable/ServersTable.js
+++ src/components/ServersTable/ServersTable.js
@@ -1,7 +1,24 @@
 import React from 'react'
 import {Table} from 'semantic-ui-react'
 import {FormattedMessage} from 'react-intl'
+import {faSun, faMoon} from '@fortawesome/free-regular-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import './ServersTable.css'
 
+
+const getDayTimeIcon = dayTime => {
+  if (!dayTime || !dayTime.includes(':')) {
+    return null
+  }
+  const hour = parseInt(dayTime.split(':')[0], 10)
+  if (hour < 6) {
+    return faMoon
+  }
+  if (hour < 18) {
+    return faSun
+  }
+  return faMoon
+}
 
 class ServersTable extends React.Component {
   render() {
@@ -29,6 +46,31 @@ class ServersTable extends React.Component {
             </Table.HeaderCell>
           </Table.Row>
         </Table.Header>
+        <Table.Body>
+          {this.props.servers.map(server => (
+            <Table.Row>
+              <Table.Cell>
+                {server.name}
+              </Table.Cell>
+              <Table.Cell>
+                {server.currentPlayers}
+              </Table.Cell>
+              <Table.Cell>
+                {server.maxPlayers}
+              </Table.Cell>
+              <Table.Cell>
+                <FontAwesomeIcon icon={getDayTimeIcon(server.dayTime)}/>
+                <span className='server-day-time'>{server.dayTime}</span>
+              </Table.Cell>
+              <Table.Cell>
+                {server.ip}
+              </Table.Cell>
+              <Table.Cell>
+                {server.version}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
       </Table>
     )
   }
