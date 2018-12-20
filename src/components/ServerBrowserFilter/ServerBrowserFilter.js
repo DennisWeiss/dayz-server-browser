bdf347763b6643d3cc91f5f3cc3c4c715e42aad4
Dennis Weiss <weiss.dennis98@yahoo.de>
2018-12-20 18:02:40 +0100

    Styling

--- src/components/ServerBrowserFilter/ServerBrowserFilter.js
+++ src/components/ServerBrowserFilter/ServerBrowserFilter.js
@@ -2,15 +2,16 @@ import React from 'react'
 import './ServerBrowserFilter.css'
 import {faFilter} from '@fortawesome/free-solid-svg-icons'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import ThreeStateCheckbox from '../../common/ThreeStateCheckbox'
 
 
 class ServerBrowserFilter extends React.Component {
   render() {
     return (
       <div className='server-browser-filter'>
-          <FontAwesomeIcon size='lg' icon={faFilter}/>
-
+        <FontAwesomeIcon size='lg' icon={faFilter}/>
         <span className='filter-heading'>Filter</span>
+        <ThreeStateCheckbox defaultChecked='positive' onClick={console.log}/>
       </div>
     )
   }
