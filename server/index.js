a84a70720553f795a20fcc847057c94a0192b862
Dennis Weiss <weiss.dennis98@yahoo.de>
2019-01-31 11:30:25 +0100

    Quick fix

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const axios = require('axios')
-const appConf = require('appConf/config')
+const appConf = require('./conf/config')
 const fs = require('fs')
 const https = require('https')
 
@@ -11,12 +11,12 @@ const port = 3001
 app.get('/', (req, res) => {
   axios
     .get(`http://api.steampowered.com/IGameServersService/GetServerList/v1/?key=${appConf.STEAM_API_KEY}&format=json&filter=appid\\221100&limit=50000`)
-    .then(res => res.send(res.data))
+    .then(response => res.send(response.data))
 })
 
 
 const startHttpServer = () => {
-  app.listen(appConf.serverPort, () => console.log(`Server running on ${appConf.SERVER_PORT}`))
+  app.listen(appConf.SERVER_PORT, () => console.log(`Server running on ${appConf.SERVER_PORT}`))
 }
 
 const startHttpsServer = (privateKeyFilename, certFilename) => {
@@ -24,7 +24,7 @@ const startHttpsServer = (privateKeyFilename, certFilename) => {
     key: fs.readFileSync(privateKeyFilename),
     cert: fs.readFileSync(certFilename)
   }, app)
-  server.listen(appConf.serverPort, () => console.log(`Server running on ${appConf.SERVER_PORT}`))
+  server.listen(appConf.SERVER_PORT, () => console.log(`Server running on ${appConf.SERVER_PORT}`))
 }
 
 if (process.argv.length >= 3 && process.argv[2] === 'https') {
